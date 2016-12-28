1b2459065af2ae73151013d4b92ded01218c591f
Fabio Romita <romitafabio@gmail.com>
2016-12-28 17:48:38 +0100

    updated version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'hotello:useful-dicts',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'A small collection of useful dictionaries.',
   // URL to the Git repository containing the source code for this package.
